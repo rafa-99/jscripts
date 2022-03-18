802cadad92e3f273694042ecc548ce21a3866504
Rafael Marçalo <raroma09@gmail.com>
2022-03-18 17:09:30 +0000

    Update 4chan-saver.js

--- 4chan-saver.js
+++ 4chan-saver.js
@@ -1,2 +1,7 @@
+/*
+ * Saves cleans up and saves
+ * 4chan threads to a PDF file
+ */
+
 document.getElementsByTagName("body")[0].innerHTML = document.querySelectorAll('[class=thread]')[0].innerHTML;
 window.print();
